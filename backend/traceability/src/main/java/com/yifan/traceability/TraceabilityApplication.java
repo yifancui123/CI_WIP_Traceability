f69323eb1b6ab82a4750b9b15a92645df062d729
Yifan Cui <yifan.cui@students.mq.edu.au>
2026-07-05 17:55:35 +1000

    Phase 1: Spring Boot skeleton

---
+++ backend/traceability/src/main/java/com/yifan/traceability/TraceabilityApplication.java
@@ -0,0 +1,13 @@
+package com.yifan.traceability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraceabilityApplication.class, args);
+	}
+
+}
---
+++ backend/traceability/src/test/java/com/yifan/traceability/TraceabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yifan.traceability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
