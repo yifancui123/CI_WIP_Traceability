24867a64cc87cbcff62f133071374f72a9a2b4fa
Yifan Cui <yifan.cui@students.mq.edu.au>
2026-09-22 22:07:52 +1000

    Added JobRepository

---
+++ backend/src/main/java/com/yifan/traceability/repository/JobRepository.java
@@ -0,0 +1,12 @@
+package com.yifan.traceability.repository;
+
+import com.yifan.traceability.entity.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface JobRepository extends JpaRepository<Job, Long> {
+    Job findByJobNumber(String jobNumber);
+    List<Job> findByPart(Part part);
+    List<Job> findByExpiryDate(LocalDate expiryDate);
+    List<Job> findByIsDeleted(boolean isDeleted);
+}
