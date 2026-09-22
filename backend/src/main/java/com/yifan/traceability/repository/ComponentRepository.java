bed7b141eab0df3cdfb674b8438e66c649dec40b
Yifan Cui <yifan.cui@students.mq.edu.au>
2026-09-22 21:15:44 +1000

    Added ComponentRepository

---
+++ backend/src/main/java/com/yifan/traceability/repository/ComponentRepository.java
@@ -0,0 +1,14 @@
+package com.yifan.traceability.repository;
+
+import com.yifan.traceability.entity.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ComponentRepository extends JpaRepository<Component, Long>{
+
+    Component findBySplitCode(String splitCode);
+    List<Component> findByQuantity(int quantity);
+    List<Component> findByStatus(String status);
+    List<Component> findByLocation(Location location);
+    List<Component> findByJob(Job job);
+}
